6b46673731c3db77df51f37d07dd6e5739b7bdae
bantheus <matheuschmidt.b@gmail.com>
2024-02-01 15:57:22 -0300

    ✨ Feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,17 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Header } from "../_components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Jhon Doe</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
